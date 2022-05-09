7920c721f1cccfb393201115da2c0eba0ad38550
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-09 13:38:54 +0300

    creating class user

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from . import db
+class User(db.Model):
+    __tablename__='users'
+
+    id=db.column(db.string,primary_Key=True)
+    id=db.column(db.string,primary_Key=True)
+    id=db.column(db.string,primary_Key=True)
