8912bbfb91361aecd0f770479012de4008200b43
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-13 13:51:55 +0300

    deployment to heroku-v2

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-from distutils.debug import DEBUG
 import os
 
 class Config:
@@ -6,32 +5,15 @@ class Config:
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
 
 
-# class ProdConfig(Config):
-    # DEBUG = False
-
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:james@localhost/pitches'
-#     DEBUG = False
-
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://','postgresql://',1)    
-
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
-
-#     DEBUG = True
+    DEBUG = False
     
+
 class DevConfig(Config):
-   '''
-   Development  configuration child class
-   Args:
-       Config: The parent configuration class with General configuration settings
-   '''
-   SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
-DEBUG = True
- 
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
+
+    DEBUG = True
+    
 
 config_options = {
 'development':DevConfig,
