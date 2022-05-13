2dd793c262d408bc2ba8c20c9b9c5bae45096713
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-13 15:13:56 +0300

    deployment to heroku-v2

--- config.py
+++ config.py
@@ -5,9 +5,16 @@ class Config:
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
 
 
+# class ProdConfig(Config):
+#     DEBUG = False
+
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
+    # if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    # SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', "postgresql://", 1)     
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
+        # sql_alchemy_conn = 'postgresql://moringa:james@host:port/db'
     DEBUG = False
-    
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:james@localhost/pitches'
