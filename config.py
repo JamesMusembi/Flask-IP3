66a8e8f400cf73195e836fd5c80a5bd7699595cb
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-13 10:54:10 +0300

    adding comment

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
 
 
 class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI ='postgresql://mxhwjrztvelttj:f78c638ad02dbae50ef11ca5e8acab88b2bdc1a3ae0e1fa274ff3186213aa009@ec2-107-22-238-112.compute-1.amazonaws.com:5432/de50v5u7j3bip6'
     DEBUG = False
     
 
